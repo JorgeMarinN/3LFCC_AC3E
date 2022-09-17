53755570b0cbf18126d585573c7d06c8f18165d8
Alfonso Cortés <alfonso.cortes@sansano.usm.cl>
2022-09-17 18:36:56 -0300

    Preliminary converter.mag
    Finished preliminary layout:
    - .tcl
    - .mag
    - lvs match (with property errors)

--- mag/tcl/converter.tcl
+++ mag/tcl/converter.tcl
@@ -3,7 +3,7 @@
 #   AC3E - UTFSM      	#
 #   Project: 3LFCC    	#
 #   Converter layout	#
-#   01-09-2022        	#
+#   17-09-2022        	#
 # #####################	#
 
 see no mvndiffusion
@@ -19,10 +19,6 @@ see no viali
 see no metal1
 see no m2contact
 see no m3contact
-see no metal3
-see no via3
-see no metal4
-see no via4
 
 #
 box 0.3um 0um 10um 10um
@@ -31,24 +27,21 @@ getcell flying_cap.mag
 box 0um 1500um 10um 1510um
 getcell power_stage.mag
 rotate -90
-# terminal 1
-box 10.3um 1467.4um 690.2um 1480um
-paint {metal3 metal5}
-box 10.3um 1470um 690.2um 1480um
+
+# connections
+box 80um 1470um 690um 1540um
 paint metal4
-box 10.5um 1470.2um 690um 1479.8um
+box 80um 1482um 690um 1540um
+paint {metal3 metal5}
+box 81um 1483um 689um 1500um
 paint {via3 via4}
 
-
-# terminal 2
-box 712.1um 1467.4um 1360.1um 1480um
-paint metal4
-box 712.1um 1470um 1360.1um 1480um
+box 820um 1470um 1300um 1540um
 paint {metal3 metal5}
-box 712.3um 1470.2um 1359.9um 1479.8um
+box 820um 1482um 1300um 1540um
+paint metal4
+box 821um 1483um 1299um 1500um
 paint {via3 via4}
-
-#paint {metal3 metal4 metal5}
 
 drc style drc(full)
 save converter.mag
