ffee0be2d0f2f11cf10585faf751a7ab70475618
Alfonso Cortés <alfonso.cortes@sansano.usm.cl>
2022-10-24 11:24:33 -0300

    Modified level_shifter, core and interleaved
    - Widened level_shifter's output and connections inside of core
    - Added 2nd guard rings connections to interleaved

--- mag/tcl/interleaved.tcl
+++ mag/tcl/interleaved.tcl
@@ -3,7 +3,7 @@
 #   AC3E - UTFSM      	#
 #   Project: 3LFCC    	#
 #   Interleaved layout	#
-#   23-10-2022        	#
+#   24-10-2022        	#
 # #####################	#
 
 # version invertida
@@ -175,6 +175,57 @@ label Vout east metal5
 # VDD
 box [expr {$midd - 20}]um [expr {$yoff + 1669.5}]um [expr {$midd + 20}]um [expr {$yoff + 1671.5}]um
 paint metal1
+
+# ## 2nd Guard Rings (Diodes) ##
+# NMOS' to VH
+box [expr {$midd + 302}]um [expr {$yoff + 1970}]um [expr {$midd + 322}]um [expr {$yoff + 2150}]um
+paint {metal2}
+box [expr {$midd - 325}]um [expr {$yoff + 1970}]um [expr {$midd - 305}]um [expr {$yoff + 2150}]um
+paint {metal2}
+box [expr {$midd + 302.15}]um [expr {$yoff + 2088.15}]um [expr {$midd + 321.85}]um [expr {$yoff + 2117.85}]um
+paint {m3contact via3 via4}
+box [expr {$midd + 302.15}]um [expr {$yoff + 2120.15}]um [expr {$midd + 321.85}]um [expr {$yoff + 2149.85}]um
+paint {m3contact via3 via4}
+box [expr {$midd - 324.85}]um [expr {$yoff + 2088.15}]um [expr {$midd - 305.05}]um [expr {$yoff + 2117.85}]um
+paint {m3contact via3 via4}
+box [expr {$midd - 324.85}]um [expr {$yoff + 2120.15}]um [expr {$midd - 305.05}]um [expr {$yoff + 2149.85}]um
+paint {m3contact via3 via4}
+# PMOS' to GND
+box [expr {$midd + 700}]um [expr {$yoff + 1653}]um [expr {$midd + 1290}]um [expr {$yoff + 1730}]um
+paint metal2
+box [expr {$midd - 1290}]um [expr {$yoff + 1653}]um [expr {$midd - 700}]um [expr {$yoff + 1730}]um
+paint metal2
+box [expr {$midd - 1380}]um [expr {$yoff + 1653}]um [expr {$midd + 1380}]um [expr {$yoff + 1663}]um
+paint {metal1 metal2}
+box [expr {$midd - 20}]um [expr {$yoff + 1710}]um [expr {$midd + 20}]um [expr {$yoff + 1780}]um
+paint metal3
+box [expr {$midd - 20}]um [expr {$yoff + 1660}]um [expr {$midd + 20}]um [expr {$yoff + 1720}]um
+paint metal2
+box [expr {$midd - 19.95}]um [expr {$yoff + 1710.05}]um [expr {$midd + 19.95}]um [expr {$yoff + 1719.95}]um
+paint {m3contact}
+# 
+box [expr {$midd + 90}]um [expr {$yoff + 1660}]um [expr {$midd + 550}]um [expr {$yoff + 1700}]um
+paint metal2
+box [expr {$midd - 550}]um [expr {$yoff + 1660}]um [expr {$midd - 90}]um [expr {$yoff + 1700}]um
+paint metal2
+box [expr {$midd - 1379.95}]um [expr {$yoff + 1653.05}]um [expr {$midd + 1379.95}]um [expr {$yoff + 1662.95}]um
+paint m2contact
+box [expr {$midd + 700}]um [expr {$yoff + 1680}]um [expr {$midd + 1290}]um [expr {$yoff + 1690}]um
+paint metal1
+box [expr {$midd - 1290}]um [expr {$yoff + 1680}]um [expr {$midd - 700}]um [expr {$yoff + 1690}]um
+paint metal1
+box [expr {$midd + 700.05}]um [expr {$yoff + 1680.05}]um [expr {$midd + 1289.95}]um [expr {$yoff + 1689.95}]um
+paint m2contact
+box [expr {$midd - 1289.95}]um [expr {$yoff + 1680.05}]um [expr {$midd - 700.05}]um [expr {$yoff + 1689.95}]um
+paint m2contact
+box [expr {$midd + 90}]um [expr {$yoff + 1680}]um [expr {$midd + 550}]um [expr {$yoff + 1690}]um
+paint metal1
+box [expr {$midd - 550}]um [expr {$yoff + 1680}]um [expr {$midd - 90}]um [expr {$yoff + 1690}]um
+paint metal1
+box [expr {$midd + 90.05}]um [expr {$yoff + 1680.05}]um [expr {$midd + 549.95}]um [expr {$yoff + 1689.95}]um
+paint m2contact
+box [expr {$midd - 549.95}]um [expr {$yoff + 1680.15}]um [expr {$midd - 90.15}]um [expr {$yoff + 1689.95}]um
+paint m2contact
 
 save interleaved.mag
 
